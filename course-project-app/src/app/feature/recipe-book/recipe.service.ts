56be80c8b7559fc164697d57689d0152cae7e70e
JJ807 <jakubstrugaa@gmail.com>
2023-01-22 14:29:23 +0100

    add ingredients to recipes

--- course-project-app/src/app/feature/recipe-book/recipe.service.ts
+++ course-project-app/src/app/feature/recipe-book/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 import { Recipe } from './model/recipe.model';
 
 @Injectable({ providedIn: 'root' })
@@ -9,12 +10,20 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAxOS1qYWt1YmstMDE4MC1jb3VzLWNvdXMtdmVnZXRhYmxlcy1wYW4uanBn.jpg?s=p7NYnOdKkx96F67qlvJO0CCRCKVkIrIh8s77KQxBzBg'
+      'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAxOS1qYWt1YmstMDE4MC1jb3VzLWNvdXMtdmVnZXRhYmxlcy1wYW4uanBn.jpg?s=p7NYnOdKkx96F67qlvJO0CCRCKVkIrIh8s77KQxBzBg',
+      [
+        new Ingredient('meat', 1),
+        new Ingredient ('French Fries', 20)
+      ]
     ),
     new Recipe(
       'Another Test Recipe',
       'This is simply a test',
-      'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAxOS1qYWt1YmstMDE4MC1jb3VzLWNvdXMtdmVnZXRhYmxlcy1wYW4uanBn.jpg?s=p7NYnOdKkx96F67qlvJO0CCRCKVkIrIh8s77KQxBzBg'
+      'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAxOS1qYWt1YmstMDE4MC1jb3VzLWNvdXMtdmVnZXRhYmxlcy1wYW4uanBn.jpg?s=p7NYnOdKkx96F67qlvJO0CCRCKVkIrIh8s77KQxBzBg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient ('Meat', 1)
+      ]
     ),
   ];
 
