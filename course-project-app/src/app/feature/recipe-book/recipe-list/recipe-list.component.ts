260966543849de4908e46fd7a8dae8d3cd5b66e7
JJ807 <jakubstrugaa@gmail.com>
2023-01-22 13:21:26 +0100

    add RecipeService

--- course-project-app/src/app/feature/recipe-book/recipe-list/recipe-list.component.ts
+++ course-project-app/src/app/feature/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../model/recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,24 +8,13 @@ import { Recipe } from '../model/recipe.model';
   styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAxOS1qYWt1YmstMDE4MC1jb3VzLWNvdXMtdmVnZXRhYmxlcy1wYW4uanBn.jpg?s=p7NYnOdKkx96F67qlvJO0CCRCKVkIrIh8s77KQxBzBg'
-    ),
-    new Recipe(
-      'Another Test Recipe',
-      'This is simply a test',
-      'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAxOS1qYWt1YmstMDE4MC1jb3VzLWNvdXMtdmVnZXRhYmxlcy1wYW4uanBn.jpg?s=p7NYnOdKkx96F67qlvJO0CCRCKVkIrIh8s77KQxBzBg'
-    ),
-  ];
-
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
+  constructor(private recipeService: RecipeService) {}
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe): void {
     this.recipeWasSelected.emit(recipe);
---
+++ course-project-app/src/app/feature/recipe-book/recipe.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './model/recipe.model';
+
+@Injectable({providedIn: 'root'})
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAxOS1qYWt1YmstMDE4MC1jb3VzLWNvdXMtdmVnZXRhYmxlcy1wYW4uanBn.jpg?s=p7NYnOdKkx96F67qlvJO0CCRCKVkIrIh8s77KQxBzBg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply a test',
+      'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAxOS1qYWt1YmstMDE4MC1jb3VzLWNvdXMtdmVnZXRhYmxlcy1wYW4uanBn.jpg?s=p7NYnOdKkx96F67qlvJO0CCRCKVkIrIh8s77KQxBzBg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
