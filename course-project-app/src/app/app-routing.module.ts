f8f65bc04941e8bfa3229eff0f119fd509ca3dc1
JJ807 <jakubstrugaa@gmail.com>
2023-02-11 00:49:08 +0100

    add new recipe-start component

--- course-project-app/src/app/app-routing.module.ts
+++ course-project-app/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeStartComponent } from './feature/recipe-book/recipe-start/recipe-start.component';
 import { RecipesComponent } from './feature/recipe-book/recipes.component';
 import { ShoppingListComponent } from './feature/shopping-list/shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [{ path: '', component: RecipeStartComponent }],
+  },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
