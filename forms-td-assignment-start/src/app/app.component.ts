f47738bda0ba8a1767615ba8d2ac43e4c8af7bb9
JJ807 <jakubstrugaa@gmail.com>
2023-03-01 21:28:02 +0100

    add form inputs and validation assignment

---
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  subscriptions = ["Basic", "Advanced", "Pro"];
+  defaultValue = "Advanced";
+
+  onSubmit(f: any) {
+    console.log(f.controls);
+  }
+}
